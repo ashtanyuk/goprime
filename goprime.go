3feb25e4978821ac7345f9200a5934d3e8a252fc
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2021-12-13 21:22:01 +0300

    Update goprime.go

--- goprime.go
+++ goprime.go
@@ -1,10 +1,10 @@
 package goprime
 
 func isPrime(number int) bool {
-   if n<=1 {
+   if number <= 1 {
      return false
    }
-   if n == 2 {
+   if number == 2 {
      return true
    }
    for i := 2; i <= number; i++ {
