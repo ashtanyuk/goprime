b9b7d0de26b6117de7d2e71b7129af87be92e226
anton <ashtanyuk@gmail.com>
2021-12-13 21:53:53 +0300

    removed equal

--- goprime.go
+++ goprime.go
@@ -7,7 +7,7 @@ func IsPrime(number int) bool {
    if number == 2 {
      return true
    }
-   for i := 2; i <= number; i++ {
+   for i := 2; i < number; i++ {
      if number % i == 0 {
         return false
      }
